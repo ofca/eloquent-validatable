29adc5f51032a5f5cb9802cdfe75f46fe232ef84
Tomasz Zeludziewicz <ofca@emve.org>
2015-04-10 12:46:36 +0200

    ...

---
+++ src/DC/EloquentValidatable/Laravel5ServiceProvider.php
@@ -0,0 +1,45 @@
+<?php namespace Dc\EloquentValidatable;
+
+use Illuminate\Support\ServiceProvider;
+
+class Laravel5ServiceProvider extends ServiceProvider {
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot() {}
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app['events']->listen('eloquent.saving*', function($model) {
+            if (in_array('DC\EloquentValidatable\ValidatableTrait', class_uses($model))) {
+                return $model->validate();
+            }
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array();
+    }
+
+}
